da5b4302c2c3a25c39a28de60829b6aca645060d
mitchdevs@gmail.com <mitch@localhost.localdomain>
2026-02-11 09:34:48 +0000

    .

--- scripts/setup-rocky-linux.sh
+++ scripts/setup-rocky-linux.sh
@@ -35,7 +35,10 @@ fi
 
 cd $rhelProvisioningScriptsDir
 
-01-disable_net_lso.sh
+# Make all .sh files executable.
+chmod +x *.sh
+
+./01-disable_net_lso.sh
 
 # Probably not needed.
 #01-install_telegraf.sh
@@ -45,20 +48,20 @@ cd $rhelProvisioningScriptsDir
 # We don't build webengine.
 #01-set-ulimit.sh
 
-01-systemsetup.sh
-02-install-xcb_util_cursor.sh
+./01-systemsetup.sh
+./02-install-xcb_util_cursor.sh
 
 # May need this; let´s see.
 #03-enable-repos.sh
 
-03-limit-avahi-interfaces.sh
-04-install-packages.sh
-04-p7zip.sh
-05-install-ninja.sh
-05-install-patchelf.sh
-05-libclang-dyn.sh
-05-libclang.sh
-05-mount-vcpkg-cache-drive.sh
+./03-limit-avahi-interfaces.sh
+./04-install-packages.sh
+./04-p7zip.sh
+./05-install-ninja.sh
+./05-install-patchelf.sh
+./05-libclang-dyn.sh
+./05-libclang.sh
+./05-mount-vcpkg-cache-drive.sh
 
 # Probably don't need these.
 #09-disable_selinux.sh
@@ -66,9 +69,9 @@ cd $rhelProvisioningScriptsDir
 #22-mqtt_broker.sh
 #30-fbx.sh
 
-30-install-conan.sh
-30-install-git.sh
-30-install_icu.sh
+./30-install-conan.sh
+./30-install-git.sh
+./30-install_icu.sh
 
 # Don't need, and requires CI network.
 #35-install-breakpad.sh
@@ -79,26 +82,26 @@ cd $rhelProvisioningScriptsDir
 #41-install-bundletool.sh
 #50-openssl_for_android_linux.sh
 
-40-install-cmake.sh
-41-install-golang.sh
-41-install-upx.sh
-41-install-vcpkg.sh
-42-install-vcpkg-ports.sh
+./40-install-cmake.sh
+./41-install-golang.sh
+./41-install-upx.sh
+./41-install-vcpkg.sh
+./42-install-vcpkg-ports.sh
 
 #51-openapi.sh
 
-60-install_protobuf.sh
-61-install_grpc.sh
-70-install_dwz.sh
+./60-install_protobuf.sh
+./61-install_grpc.sh
+./70-install_dwz.sh
 
 # Not needed since we´re running our own VM.
 #70-install_QemuGA.sh
 
-90-bootstrap-autostart.sh
+./90-bootstrap-autostart.sh
 
 #90-firebird.sh
 
-90-install-ffmpeg.sh
+./90-install-ffmpeg.sh
 
 #90-install-oracle.sh
 #90-mimer.sh
@@ -106,7 +109,7 @@ cd $rhelProvisioningScriptsDir
 # Probably don´t need to run this every time?
 #99-cleanup.sh
 
-99-enable_test_stacktraces.sh
+./99-enable_test_stacktraces.sh
 
 #99-network-test.sh
 
